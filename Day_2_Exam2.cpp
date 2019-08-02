43b5683fa8d781c1c839cd6bbffa8b086bb8f4cb
Mrcer <2969944867@qq.com>
2019-08-02 13:50:57 +0000

    Still some bugs

---
+++ Day_2_Exam2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+const int MAX = 101;
+int a[MAX], f[MAX][MAX], N, ans;
+
+inline int trans(int i) {
+	return (N+i)%N;
+}
+
+void read() {
+	cin >> N;
+	for(int i = 0;i < N;++i) {
+		cin >> a[i];
+		a[i] = a[i]&1;
+		f[i][i] = a[i];
+	}
+}
+
+void dp() {
+	for(int len = N-1;len > 0;--len)
+		for(int i = 0;i < N;++i)
+			f[i][trans(i+len)] = max(-f[i-1][trans(i+len)] + a[i], -f[i][trans(i+len+1)] + a[trans(i+len)]);
+}
+
+int main() {
+	read();
+	dp();
+	for(int i = 0;i < N;++i)
+		ans = max(f[trans(i-1)][trans(i+1)], ans);
+	cout << ans << endl;
+}
